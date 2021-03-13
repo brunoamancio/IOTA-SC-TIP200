cc82518d2df9871fc3b8d918280d07cc828fc958
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-14 00:28:07 +0100

    Bumped iota-sc-hname-generator to 1.1.1. Added results getter and param adder functions. Started implementing tip_100.

--- smartcontract/rust/iota_sc_utils/src/results.rs
+++ smartcontract/rust/iota_sc_utils/src/results.rs
@@ -1,19 +1,20 @@
 use wasmlib::*;
 
-
-macro_rules! add_impl_pub_fns {
+macro_rules! add_impl_pub_setter_fns {
     ($set_func:ident, $get_func:ident, $param_type:ty) => {
         /// Tries to set result. Does nothing if it can't find it.
-        pub fn $set_func<TContext : ScBaseContext>(param_name : &str, param_value : $param_type, ctx: &TContext) {
+        pub fn $set_func<TContext : ScBaseContext>(param_name : &str, param_value : $param_type, ctx : &TContext) {
             ctx.results().$get_func(param_name).set_value(param_value);
         }
     };
 }
 
+// ---------------------------    Setter functions    -------------------------------------
+
 // Primitive types
-add_impl_pub_fns!(set_string, get_string, &str);
-add_impl_pub_fns!(set_int64, get_int64, i64);
-add_impl_pub_fns!(set_bytes, get_bytes, &[u8]);
+add_impl_pub_setter_fns!(set_string, get_string, &str);
+add_impl_pub_setter_fns!(set_int64, get_int64, i64);
+add_impl_pub_setter_fns!(set_bytes, get_bytes, &[u8]);
 /// Tries to set result. Does nothing if it can't find it.
 pub fn set_bool<TContext : ScBaseContext>(param_name : &str, param_value : bool, ctx: &TContext) {
     let bool_value = to_u8(param_value);
@@ -32,11 +33,49 @@ fn to_u8(param_value : bool) -> Vec<u8> {
 
 
 // ISCP Types
-add_impl_pub_fns!(set_agent_id, get_agent_id, &ScAgentId);
-add_impl_pub_fns!(set_address, get_address, &ScAddress);
-add_impl_pub_fns!(set_request_id, get_request_id, &ScRequestId);
-add_impl_pub_fns!(set_hname, get_hname, ScHname);
-add_impl_pub_fns!(set_hash, get_hash, &ScHash);
-add_impl_pub_fns!(set_contract_id, get_contract_id, &ScContractId);
-add_impl_pub_fns!(set_color, get_color, &ScColor);
-add_impl_pub_fns!(set_chain_id, get_chain_id, &ScChainId);
+add_impl_pub_setter_fns!(set_agent_id, get_agent_id, &ScAgentId);
+add_impl_pub_setter_fns!(set_address, get_address, &ScAddress);
+add_impl_pub_setter_fns!(set_request_id, get_request_id, &ScRequestId);
+add_impl_pub_setter_fns!(set_hname, get_hname, ScHname);
+add_impl_pub_setter_fns!(set_hash, get_hash, &ScHash);
+add_impl_pub_setter_fns!(set_contract_id, get_contract_id, &ScContractId);
+add_impl_pub_setter_fns!(set_color, get_color, &ScColor);
+add_impl_pub_setter_fns!(set_chain_id, get_chain_id, &ScChainId);
+
+// ---------------------------    Getter functions    -------------------------------------
+
+macro_rules! add_impl_pub_getter_fns {
+    ($get_func:ident, $return_type:ty) => {
+        pub fn $get_func(param_name : &str, immutablemap : ScImmutableMap) -> $return_type {
+            immutablemap.$get_func(param_name).value()
+        }
+    }
+}
+
+// Primitive types
+add_impl_pub_getter_fns!(get_string, String);
+add_impl_pub_getter_fns!(get_int64, i64);
+add_impl_pub_getter_fns!(get_bytes, Vec<u8>);
+
+pub fn get_bool(param_name : &str, immutablemap : ScImmutableMap) -> bool {
+    let param_value = immutablemap.get_bytes(param_name).value();
+    let bool_value = to_bool(param_value);
+    bool_value
+}
+
+fn to_bool(bytes_vector : Vec<u8>) -> bool {
+    match bytes_vector.get(0) {
+        Some(byte) => *byte == 1,
+        None => panic!("Could not convert byte to bool")
+    }
+}
+
+// ISCP Types
+add_impl_pub_getter_fns!(get_agent_id, ScAgentId);
+add_impl_pub_getter_fns!(get_address, ScAddress);
+add_impl_pub_getter_fns!(get_request_id, ScRequestId);
+add_impl_pub_getter_fns!(get_hname, ScHname);
+add_impl_pub_getter_fns!(get_hash, ScHash);
+add_impl_pub_getter_fns!(get_contract_id, ScContractId);
+add_impl_pub_getter_fns!(get_color, ScColor);
+add_impl_pub_getter_fns!(get_chain_id, ScChainId);
--- smartcontract/rust/src/constants.rs
+++ smartcontract/rust/src/constants.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
-use iota_sc_hname_generator::generate_hname;
+use iota_sc_hname_generator::generate_schname;
 use wasmlib::ScHname;
 
 pub const SC_NAME : &str  = "erc_721";
-pub const HNAME_SC_NAME : ScHname  = generate_hname!("erc_721");
+pub const HNAME_SC_NAME : ScHname  = generate_schname!("erc_721");
+
+pub const MY_DUMMY_FN : &str  = "my_dummy_fn";
+pub const HNAME_MY_DUMMY_FN : ScHname  = generate_schname!("my_dummy_fn");
